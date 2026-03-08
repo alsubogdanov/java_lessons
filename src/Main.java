e0be5bc9bb199f9ee7647f163c5d730c46df8b12
alsu <alsousha@mail.ru>
2026-03-08 12:00:48 +0200

    Дата, время, таймзоны LocalDate, LocalTime, LocalDateTime

--- src/Main.java
+++ src/Main.java
@@ -1,172 +1,104 @@
-import com.sun.net.httpserver.Request;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-static HttpClient client = HttpClient.newHttpClient();
-
-public static void task0_2(){
-    try{
-        // Создаем новую колоду
-        String newDeckResponse = fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-        System.out.println("Ответ сервера при создании колоды:");
-        System.out.println(newDeckResponse);
-
-        // извлечение deck_id из fetchBody
-        String deckId = getDataFromStr(newDeckResponse, "\"deck_id\": \"", 12, "\"");
-        System.out.println("Deck ID: " + deckId);
-
-        // Вытягиваем 1 карту
-        String drawOneResponse = fetch("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=1");
-        System.out.println("\nВытянутые карты (JSON):");
-
-//        // извлекаем value
-        String value = getDataFromStr(drawOneResponse, "\"value\": \"", 10, "\"");
-
-        // извлекаем suit
-        String suit = getDataFromStr(drawOneResponse, "\"suit\": \"", 9, "\"");
-
-        // извлекаем remaining
-        String remaining = getDataFromStr(drawOneResponse, "\"remaining\":", 12, "}");
-
-        // вывод результата
-        System.out.println("\nYou got: " + value + " of " + suit);
-        System.out.println("Cards remaining: " + remaining);
-        System.out.println(drawOneResponse);
-
-    }catch(IOException|InterruptedException e){
-        System.out.println(e.getMessage());
-    }
+public static void task0_1() throws IOException{
+    String dateStr = Files.readString(Path.of("birthday.txt"));
+    System.out.println(dateStr);
+    LocalDate date = LocalDate.parse(dateStr);
+    System.out.println(date);
+    System.out.println(date.getDayOfWeek());
 }
-public static void task0_3(){
-    try{
-        // Создаем новую колоду
-        String newDeckResponse = fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+public static void task0_2() throws IOException{
+    String filename = "current_time.txt";
+    LocalTime time = LocalTime.now();
+    System.out.println(time);
+    Files.writeString(Path.of(filename), time.toString());
 
-        // извлечение deck_id из fetchBody
-        String deckId = getDataFromStr(newDeckResponse, "\"deck_id\": \"", 12, "\"");
-        System.out.println("Deck ID: " + deckId);
-
-        // Вытягиваем 1 карту
-        String drawFirstCardResponse = fetch("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=1");
-        String[] firstCardData = parseCard(drawFirstCardResponse);
-        String drawSecondCardResponse = fetch("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=1");
-        String[] secondCardData = parseCard(drawSecondCardResponse);
-
-        int cardVal1 = getCardIntValue(firstCardData[0]);
-        int cardVal2 = getCardIntValue(secondCardData[0]);
-
-        // вывод результата
-        System.out.println(String.format("Card 1: %s of %s", firstCardData[0], firstCardData[1]));
-        System.out.println(String.format("Card 2: %s of %s", secondCardData[0], secondCardData[1]));
-        if(cardVal1 > cardVal2){
-            System.out.println("Winner: Card 1");
-        }
-        else if(cardVal2 > cardVal1){
-            System.out.println("Winner: Card 2");
-        }
-        else{
-            System.out.println("Tie!");
-        }
-        System.out.println("Cards remaining: " + secondCardData[2]);
-
-    }catch(IOException|InterruptedException|NumberFormatException e){
-        System.out.println(e.getMessage());
-    }
+    String timeStr = Files.readString(Path.of(filename));
+    LocalTime time2 = LocalTime.parse(timeStr);
+    System.out.println(time2);
 }
-public static void task0_5(){
-    try{
-        // Создаем новую колоду
-        String newDeckResponse = fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+public static void task0_3() throws  IOException, DateTimeParseException{
+    String filename = "meeting_times.txt";
+    //1
+    String times = Files.readString(Path.of(filename));
+    String[] arr = times.split("\\R");
+    LocalTime time1 = LocalTime.parse(arr[0]);
+    LocalTime time2 = LocalTime.parse(arr[1]);
 
-        // извлечение deck_id из fetchBody
-        String deckId = getDataFromStr(newDeckResponse, "\"deck_id\": \"", 12, "\"");
-        System.out.println("Your hand:");
-        String cardVal="";
-        do {
-            // Вытягиваем карту
-            String drawFirstCardResponse = fetch("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=1");
-            String[] firstCardData = parseCard(drawFirstCardResponse);
-            cardVal = firstCardData[0];
-            System.out.println(cardVal + " of " + firstCardData[1]);
-        }while (!cardVal.equals("ACE"));
-
-    }catch(IOException|InterruptedException|NumberFormatException e){
-        System.out.println(e.getMessage());
+    //2
+//    List<String> times = Files.readAllLines(Path.of(filename));
+//    LocalTime time1 = LocalTime.parse(times.get(0));
+//    LocalTime time2 = LocalTime.parse(times.get(1));
+//
+    if(time1.isAfter(time2))
+        System.out.println(time1);
+    else
+        System.out.println(time2);
+}
+public static void task0_5() throws IOException, DateTimeParseException, IllegalArgumentException{
+    String startDatesFilename = "start_dates.txt";
+    String endDatesFilename = "end_dates.txt";
+    List<String> startDates = Files.readAllLines(Path.of(startDatesFilename));
+    List<String> endDates = Files.readAllLines(Path.of(endDatesFilename));
+    if(startDates.size()!= endDates.size()) {
+        throw new IllegalArgumentException("not correct data in files");
+    }
+    for(int i=0; i<startDates.size(); i++){
+        LocalDate d1 = LocalDate.parse(startDates.get(i).trim());
+        LocalDate d2 = LocalDate.parse(endDates.get(i).trim());
+        if(d2.isBefore(d1)){
+            throw new IllegalArgumentException("End date before start date for event "+(i+1));
+        }
+        long days = ChronoUnit.DAYS.between(d1,d2);
+        System.out.println("Event# " + (i+1)+" lasts "+ days+ " days");
     }
 }
 
-public static String getDataFromStr(String str, String startVal, int offset, String endVal){
-    int start = str.indexOf(startVal) + offset;
-    int end = str.indexOf(endVal, start);
+public static void task0_6(LocalDate date) throws IOException, DateTimeParseException, IllegalArgumentException {
+    String event_datesFilename = "event_dates.txt";
+    String event_namesFilename = "event_names.txt";
+    List<String> event_dates = Files.readAllLines(Path.of(event_datesFilename));
+    List<String> event_names = Files.readAllLines(Path.of(event_namesFilename));
+    if (event_names.size() != event_dates.size())
+        throw new IllegalArgumentException("not correct data in files");
 
-    String res = str.substring(start, end);
+    //1
+//    for (int i = 0; i < event_dates.size(); i++) {
+//        LocalDate currentDate = LocalDate.parse(event_dates.get(i).trim());
+//        if (currentDate.equals(date)) {
+//            System.out.println(currentDate + " -> " + event_names.get(i));
+//            return;
+//        }
+//    }
+//    System.out.println("No event on this date");
 
-    return res;
+    //2
+    HashMap<LocalDate, String> events = new HashMap<>();
+    for(int i=0; i<event_dates.size(); i++){
+        events.put(LocalDate.parse(event_dates.get(i).trim()),event_names.get(i).trim());
+    }
+    if (events.containsKey(date)){
+        System.out.println(date + " -> " + events.get(date));
+    }else {
+        System.out.println("No event on this date");
+    }
 }
-
-public static String fetch(String url) throws IOException, InterruptedException{
-    HttpRequest req = HttpRequest.newBuilder(URI.create(url)).GET().build();
-    HttpResponse<String> res = client.send(req, HttpResponse.BodyHandlers.ofString());
-
-    return res.body();
-}
-
-public static String[] parseCard(String str){
-    // извлекаем value
-    String value = getDataFromStr(str, "\"value\": \"", 10, "\"");
-
-    // извлекаем suit
-    String suit = getDataFromStr(str, "\"suit\": \"", 9, "\"");
-
-    // извлекаем remaining
-    String remaining = getDataFromStr(str, "\"remaining\":", 12, "}");
-    return new String[]{value, suit, remaining};
-}
-
-public static int getCardIntValue(String cardValue){
-    int res = switch (cardValue){
-        case "JACK" -> 11;
-        case "QUEEN" -> 12;
-        case "KING" -> 13;
-        case "ACE" -> 14;
-        default -> Integer.parseInt(cardValue);
-    };
-    return res;
-}
-
-public static String getCardStringValue(int cardValue){
-    String res = switch (cardValue){
-        case 11 -> "JACK";
-        case 12 -> "QUEEN";
-        case 13 -> "KING";
-        case 14 -> "ACE";
-        default -> cardValue+"";
-    };
-    return res;
-}
-
-
-
 
 public static void main(String[] args)  {
-//    task0_2();
-//    task0_3();
-    task0_5();
 
-
-
-
-
-
-
-
-
-
-
-
-
+    try {
+//        task0_1();
+//        task0_2();
+//        task0_3();
+//        task0_5();
+        LocalDate d = LocalDate.parse("2026-06-15");
+        task0_6(d);
+    }catch (IOException|DateTimeParseException e){
+        System.out.println(e.getMessage());
     }
+
+
+
+
+
+
+
+}
